e3c884371b0d879d561b683a707742f1a0633096
cofmass <963072095@qq.com>
2025-04-06 17:07:00 +0800

    新增yaml

--- src/main/java/com/cofmass/clothRoomBackend/ClothRoomBackendApplication.java
+++ src/main/java/com/cofmass/clothRoomBackend/ClothRoomBackendApplication.java
@@ -3,11 +3,13 @@ package com.cofmass.clothRoomBackend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 @SpringBootApplication
 @MapperScan("com.cofmass.clothRoomBackend.mapper")
+@PropertySource("classpath:/application-prod.yml")
 public class ClothRoomBackendApplication {
 
     public static void main(String[] args) {
